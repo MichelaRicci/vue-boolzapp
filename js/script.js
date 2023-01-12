e901135147c09d80b818414338f21cd0546d0c3b
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-12 17:49:46 +0100

    contacts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log('Vue Ok');
+
+const app = Vue.createApp({
+
+    data(){
+
+        return{
+
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+
+        contacts: [
+            {
+              name: 'Michele',
+              avatar: '_1',
+            },
+            {
+              name: 'Fabio',
+              avatar: '_2',
+            },
+            {
+              name: 'Samuele',
+              avatar: '_3',
+            },
+            {
+              name: 'Luisa',
+              avatar: '_4',
+            },
+        ]}
+    }
+  
+  }).mount('#root');
+  
